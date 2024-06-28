5c076248f6af561d761b2531f0dfdf5646d9894f
eddieb <eddiewird@gmail.com>
2024-06-27 21:49:31 -0400

    chore: Update file path in main.py

--- main.py
+++ main.py
@@ -3,7 +3,7 @@ import webbrowser
 
 # HTML code
 
-file_path = "html.txt"  # Replace with the actual file path
+file_path = "html.txt"  # Replace with the actual file path!!
 
 with open(file_path, "r") as file:
     plain_text = file.read()
@@ -14,23 +14,21 @@ html = plain_text
 
 soup = BeautifulSoup(html, "html.parser")
 
-# Find all <div> elements with the class "d2l-htmlblock-untrusted"
+
 div_elements = soup.find_all('div', class_='d2l-htmlblock-untrusted')
 
 google_doc = []
 google_slide = []
-# Iterate through each <div> element
+
 for div in div_elements:
-    # Extract the content of the html attribute
     d2l_block = div.find('d2l-html-block')
     if d2l_block and 'html' in d2l_block.attrs:
         embedded_html = d2l_block['html']
         
-        # Convert the embedded HTML from HTML entities back to standard HTML
+
         embedded_soup = BeautifulSoup(embedded_html, "html.parser")
         
-        # Find all <a> elements in the embedded HTML and print their href attributes
-        
+      
         a_elements = embedded_soup.find_all('a')
         for a in a_elements:
             link = a['href']
---
+++ readme.md
@@ -0,0 +1,3 @@
+Steps
+1. Paste your Brightspace html code (from inspect element) into a file
+2. Replace the filename in the file_path variable to your filename
